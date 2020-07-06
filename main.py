48a9eaf3de277f1590f9fa5552d940fceb3945d9
MikkoGit <leivo.mikko@gmail.com>
2020-07-06 15:44:32 +0300

    päivitetty loop

--- main.py
+++ main.py
@@ -1,54 +1,40 @@
 #Funktiot eli kysymykset
-
-#Kysymys1
-
-        # onko mikolla isot jalat:
-        # a)on
-        # b)on tosi isot
-        # c)no ihan helvetin isot
-        # d) pienet ja kauniit
-#Kysymys2
-
-#Kysymys3
-def kysymys3(kysymyslista):
-    vastaus = input("Onko EC2 Amazonin palvelu?:\na)Ehkä\nb)Kyllä\nc)Ei\nVastaa a, b tai c: ").lower()
-    if vastaus =="a" or vastaus=="b" or vastaus=="c":
-        if vastaus =="b":
-            oikeat_vastaukset +=1
-            print("Vastasit oikein")
-    else:
-        print("vastaa a, b tai c")
-#Kysymys4
-
-#Kysymys5
-
-
-
-
 oikeat_vastaukset = 0
 kysymyslista = [1,2,3,4,5]
 import random
 random.shuffle(kysymyslista)
 
+#Kysymys1
+def kysymys1(kysymyslista, oikeat_vastaukset):
+    vastaus = input("Onko EC2 Amazonin palvelu?:\na)Ehkä\nb)Kyllä\nc)Ei\nVastaa a, b tai c: ").lower()
+    if vastaus =="a" or vastaus=="b" or vastaus=="c":
+        if vastaus =="b":
+            oikeat_vastaukset =oikeat_vastaukset + 1
+            print("Vastasit oikein")
+    else:
+        print("vastaa a, b tai c")
+
+#Kysymys2
 
 while True:
     kysymys = input("Hei haluatko pelata? Vastaa K(yllä) tai E(i): ").lower()
     if kysymys.startswith("k"):
         for numero in kysymyslista:
             if numero == 1:
-                kysymys1(kysymyslista)
+                kysymys1(kysymyslista, oikeat_vastaukset)
             elif numero == 2:
-                kysymys2(kysymyslista)
+                kysymys2(kysymyslista, oikeat_vastaukset)
             elif numero == 3:
-                kysymys3(kysymyslista)
+                kysymys3(kysymyslista, oikeat_vastaukset)
             elif numero == 4:
-                kysymys4(kysymyslista)
+                kysymys4(kysymyslista, oikeat_vastaukset)
             elif numero == 5:
-                kysymys5(kysymyslista)
+                kysymys5(kysymyslista, oikeat_vastaukset)
             else:
-                print(f'Hieno homma, vastasit onnistuneesti yhteensä {oikeat_vastaukset} kysymykseen.')
-                lopeta = input('Paina Enter ja poistu pelistä.')
                 break
+
+        print(f'Hieno homma, vastasit onnistuneesti yhteensä {oikeat_vastaukset} kysymykseen.')
+        lopeta = input('Paina Enter ja poistu pelistä.')
     elif kysymys.startswith("e"):
         break
     else:
